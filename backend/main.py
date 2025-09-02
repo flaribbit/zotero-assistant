df6da292f4680a50e9b8c5a4db3fc23d63b3f0ab
flaribbit <1149761294@qq.com>
2025-09-02 11:25:28 +0800

    fastapi启动

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from backend") # This is the backend service
+from fastapi import FastAPI
+import uvicorn
+import logging
+
+logger = logging.getLogger("backend")
+app = FastAPI()
+
+
+@app.get("/")
+def get_root():
+    return {"message": "Hello, FastAPI!"}
+
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
 
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig(level=logging.WARN, format="%(asctime)s [%(levelname)s] %(message)s")
+    logger.setLevel(logging.INFO)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
